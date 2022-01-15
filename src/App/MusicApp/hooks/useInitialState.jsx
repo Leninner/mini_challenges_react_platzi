d56dd1804e84f6e9b397bf3823371b2d610e9067
leninner <mazabandalenin180@gmail.com>
2022-01-15 10:50:35 -0500

    Update

--- src/App/MusicApp/hooks/useInitialState.jsx
+++ src/App/MusicApp/hooks/useInitialState.jsx
@@ -5,9 +5,7 @@ export const useInitialState = () => {
   const [searchArtist, setSearchArtist] = useState('');
 
   const fetchData = async (url) => {
-    const response = await axios.get(url, {
-      mode: 'no-cors',
-    });
+    const response = await axios.get(url);
 
     return response;
   };
@@ -20,8 +18,12 @@ export const useInitialState = () => {
     e.preventDefault();
 
     fetchData(
-      `https://api.musixmatch.com/ws/1.1/artist.search?q_artist=${searchArtist}&page_size=5&apikey=	1560a82e79bee9e84d481008bf582bb1`
-    ).then((response) => console.log(response));
+      `https://api.musixmatch.com/ws/1.1/artist.search?q_artist=${encodeURI(
+        searchArtist
+      )}&page_size=5&apikey=1560a82e79bee9e84d481008bf582bb1`
+    )
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
 
     setSearchArtist('');
   };
