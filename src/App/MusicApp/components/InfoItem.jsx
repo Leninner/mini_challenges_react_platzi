bba6f60e7e8246ba4227df80adaa92003a829867
leninner <mazabandalenin180@gmail.com>
2022-01-16 17:39:51 -0500

    Update

--- src/App/MusicApp/components/InfoItem.jsx
+++ src/App/MusicApp/components/InfoItem.jsx
@@ -8,6 +8,10 @@ export const InfoItem = ({ albumTitle, cover_small, rank, artistName, artistId, 
         <img src={artistPicture} alt={artistName} className='w-10 rounded-full' />
       </div>
 
+      <span className='font-medium text-gray-500'>
+        Rank: <span className='font-bold text-blue-sky'>{rank}</span>
+      </span>
+
       <div className='flex items-center justify-between'>
         <p className='font-medium text-gray-500 w-60 lg:w-9/12'>
           Album: <span className='font-bold text-red-500'>{albumTitle}</span>
@@ -16,10 +20,6 @@ export const InfoItem = ({ albumTitle, cover_small, rank, artistName, artistId, 
           <img src={cover_small} alt='' className='w-10 border rounded-full border-slate-400' />
         </a>
       </div>
-
-      <p className='font-medium text-gray-500'>
-        Rank: <span className='font-bold text-blue-sky'>{rank}</span>
-      </p>
     </div>
   );
 };
